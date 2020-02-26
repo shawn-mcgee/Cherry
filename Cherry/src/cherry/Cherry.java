0987b4316ea6c6207f51d1a29225ec1a5889d916
McGeek07 <s.mcgeek07@gmail.com>
2020-02-26 10:35:06 -0700

    0.0.1

--- Cherry/src/cherry/Cherry.java
+++ Cherry/src/cherry/Cherry.java
@@ -1,9 +1,16 @@
 package cherry;
 
-import blue.Blue;
+import blue.core.Engine;
+import blue.util.Version;
 
 public class Cherry {
+	public static final Version
+		VERSION = new Version("Cherry", 0, 0, 1);
+	
 	public static void main(String[] args) {
-		System.out.println(Blue.VERSION);
+		Engine.getConfiguration().set(Engine.WINDOW_TITLE, VERSION);
+		System.out.println(VERSION);
+		
+		Engine.init();
 	}
 }
