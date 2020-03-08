a3f4a3a14742a0e495ba6810773ded5b771902da
McGeek07 <s.mcgeek07@gmail.com>
2020-03-08 10:42:08 -0600

    Working on Collisions

--- Cherry/src/cherry/game/Entity.java
+++ Cherry/src/cherry/game/Entity.java
@@ -26,7 +26,7 @@ public class Entity implements Renderable, Updateable {
 		pixel = new Vector2.Mutable(),
 		speed = new Vector2.Mutable();
 	public float
-		size = .5f;
+		size = .25f;
 	
 	public float x() {
 		return pixel.x();
@@ -55,7 +55,36 @@ public class Entity implements Renderable, Updateable {
 				size * FULL_H,
 				false
 				);
+		
+		if(segment_a != null) {
+			context.color(Color.GREEN);
+			context.line(
+					localToPixel(segment_a.v1),
+					localToPixel(segment_a.v2));
+		}
+		
+		if(segment_b != null) {
+			context.color(Color.BLUE);
+			context.line(
+					localToPixel(segment_b.v1),
+					localToPixel(segment_b.v2));
+		}
+		
+		if(point != null) {
+			context.color(Color.RED);
+			context.line(
+					localToPixel(local),
+					localToPixel(point)
+					);
+		}
 	}
+	
+
+	Segment
+		segment_a = null,
+		segment_b = null;
+	Vector2
+		point = null;
 	
 	@Override
 	public void onUpdate(UpdateContext context) {
@@ -65,24 +94,29 @@ public class Entity implements Renderable, Updateable {
 		int
 			i = (int)local.x(),
 			j = (int)local.y();
-		Segment
-			segment_a = null,
-			segment_b = null; 
 		
 		if(dx < 0) {
-			if(room.get_wall(i - 1, j) != null)
+			if(room.get_wall(i - 1, j) != null) {
 				segment_a = new Segment(i    , j    , i    , j + 1);
-			else if(dy < 0 && room.get_wall(i - 1, j - 1) != null)
+				System.out.println("case 1");
+			} else if(dy < 0 && room.get_wall(i - 1, j - 1) != null) {
 				segment_a = new Segment(i - 1, j    , i    , j    );
-			else if(dy > 0 && room.get_wall(i - 1, j + 1) != null)
-				segment_a = new Segment(i - 1, j + 1, i    , j + 1);				
+				System.out.println("case 2");
+			} else if(dy > 0 && room.get_wall(i - 1, j + 1) != null) {
+				segment_a = new Segment(i - 1, j + 1, i    , j + 1);
+				System.out.println("case 3");
+			}
 		} else if(dx > 0) {
-			if(room.get_wall(i + 1, j) != null)
+			if(room.get_wall(i + 1, j) != null) {
 				segment_a = new Segment(i + 1, j    , i + 1, j + 1);
-			else if(dy < 0 && room.get_wall(i + 1, j - 1) != null)
+				System.out.println("case 4");
+			} else if(dy < 0 && room.get_wall(i + 1, j - 1) != null) {
 				segment_a = new Segment(i + 1, j    , i + 2, j    );
-			else if(dy > 0 && room.get_wall(i + 1, j + 1) != null)
+				System.out.println("case 5");
+			} else if(dy > 0 && room.get_wall(i + 1, j + 1) != null) {
 				segment_a = new Segment(i + 1, j + 1, i + 2, j + 1);
+				System.out.println("case 6");
+			}
 		}
 		
 		if(dy < 0) {
@@ -107,69 +141,59 @@ public class Entity implements Renderable, Updateable {
 			delta = Float.POSITIVE_INFINITY;
 		
 		if(segment_a != null) {
+			Vector2 
+				c = Vector.add(local, dx, dy);	
 		    float 
-		    	t = Vector.s_pro(Vector.sub(local, segment_a.v1), segment_a.dv);
+		    	t = Vector.s_pro(Vector.sub(c, segment_a.v1), segment_a.dv);
 		    Vector2 
 		    	p = Vector.add(segment_a.v1, Vector.mul(segment_a.dv, Util.clamp(t, 0f, 1f))); 
 		    float 
-		    	d = Vector.dot(Vector.sub(local, p));
+		    	d = Vector.dot(Vector.sub(c, p));
 		    if(d < delta) {
 		    	point = p;
 		    	delta = d;
+		    	
+		    	this.point = p;
 		    }
 		}
 		
 		if(segment_b != null) {
+			Vector2
+				c = Vector.add(local, dx, dy);
 		    float 
-		    	t = Vector.s_pro(Vector.sub(local, segment_b.v1), segment_b.dv);
+		    	t = Vector.s_pro(Vector.sub(c, segment_b.v1), segment_b.dv);
 		    Vector2
 		    	p = Vector.add(segment_b.v1, Vector.mul(segment_b.dv, Util.clamp(t, 0f, 1f)));
 		    float 
-		    	d = Vector.dot(Vector.sub(local, p));
+		    	d = Vector.dot(Vector.sub(c, p));
 		    if(d < delta) {
 		    	point = p;
 		    	delta = d;
+		    	
+		    	this.point = p;
 		    }
 		}
 		
 		if(point != null && delta < size * size) {
-			float
-				tx = 1f,
-				ty = 1f;
-			float c0 = 
-					(point.x() * point.x()) - (2 * (point.x() * local.x())) + (local.x() * local.x()) + 
-					(point.y() * point.y()) - (2 * (point.y() * local.y())) + (local.y() * local.y()) -
-					size * size;			
-			//solve for dx
-			{
-				float a0 = dx * dx;
-				if(a0 != 0) {
-					float b0 = 2 * dx * (local.x() - point.x());
-					float
-						b1 = b0 / a0,
-						c1 = c0 / a0,
-						q0 = b1 / 2 ,
-						q1 = (float)Math.sqrt(q0 * q0 - c1);
-					tx = Util.clamp(Math.min(-q0 + q1, -q0 - q1), 0f, 1f);
-				}
-			}
-			
-			//solve for dy
-			{
-				float a0 = dy * dy;
-				if(a0 != 0) {
-					float b0 = 2 * dy * (local.y() - point.y());
-					float
-						b1 = b0 / a0,
-						c1 = c0 / a0,
-						q0 = b1 / 2 ,
-						q1 = (float)Math.sqrt(q0 * q0 - c1);
-					ty = Util.clamp(Math.min(-q0 + q1, -q0 - q1), 0f, 1f);
-				}
-			}
-			
-			dx *= tx;
-			dy *= ty;
+			float a0 = (dx * dx) + (dy * dy);			
+			if(a0 != 0) {
+				float 
+					b0 = 2 * dx * (local.x() - point.x()) + 2 * dy * (local.y() - point.y()),
+					c0 = 
+						(point.x() * point.x()) - (2 * (point.x() * local.x())) + (local.x() * local.x()) + 
+						(point.y() * point.y()) - (2 * (point.y() * local.y())) + (local.y() * local.y()) -
+						size * size;
+				float
+					b1 = b0 / a0,
+					c1 = c0 / a0,
+					q0 = b1 / 2 ,
+					q1 = (float)Math.sqrt(q0 * q0 - c1),
+					t0 = -q0 - q1,
+					t1 = -q0 + q1;
+				System.out.println(t0 + ", " + t1);
+				dx *= t0;
+				dy *= t0;
+			}			
 		}
 		
 		Vector.m_add(local, dx, dy);
